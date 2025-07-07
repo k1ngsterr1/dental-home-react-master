4a9c0cec68bd937d67fd8c0fdfe938bb92706d1b
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2025-07-07 23:48:05 +0500

    Remove Helmet script management from Layout component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Bounce } from "react-awesome-reveal";
-import { Helmet } from "react-helmet";
 
 const whatsapp = require("../../assets/whatsapp.webp");
 
@@ -15,33 +14,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <div className="layout">
-      <Helmet>
-        <script type="text/javascript">
-          {`
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.webvisor.org/metrika/tag_ww.js", "ym");
-            
-            ym(98696884, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true,
-              webvisor:true
-            });
-          `}
-        </script>
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/98696884"
-              style={{ position: "absolute", left: "-9999px" }}
-              alt=""
-            />
-          </div>
-        </noscript>
-      </Helmet>
       {children}
       <a
         href="https://wa.me/79252229022?text=Здравствуйте!%20Хочу%20записаться%20к%20вам%20в%20клинику."
