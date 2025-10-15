96220311756e053e1de898bbfb1a702dc15e1428
k1ngsterr1 <swerss564@gmail.com>
2025-10-16 04:06:18 +0500

    Add marusya.jpg image asset for enhanced visual contentДо

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -21,6 +21,7 @@ const denisImage = require("../../assets/denis.webp");
 const andreyImage = require("../../assets/andrey_andreevich.jpg");
 const annaImage = require("../../assets/anna_sergeevna.jpg");
 const ekaterinaImage = require("../../assets/ekaterina.jpg");
+const marusyaImage = require("../../assets/marusya.jpg");
 
 const tatjanaImage = require("../../assets/tanya_new.webp");
 const tatjanaImage2 = require("../../assets/tanya.webp");
@@ -226,6 +227,19 @@ const Gallery: React.FC<GalleryProps> = ({ openModal }) => {
             </div>
             <h3 className="doctor-name">Терехова Елена Юрьевна</h3>
             <p className="doctor-position">Стоматолог терапевт</p>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="dentist-card-2">
+            <div className="button-image-container">
+              <button className="doctor-btn" onClick={openModal}>
+                <span className="text">Записаться</span>
+              </button>
+              <img src={marusyaImage} alt="Doctor" className="doctor-image" />
+            </div>
+            <h3 className="doctor-name">Пахалян Маруся Акоповна</h3>
+            <p className="doctor-position">Врач стоматолог детский</p>
           </div>
         </SwiperSlide>
         {/* <SwiperSlide>
